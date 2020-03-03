ba81f6f7ee16077bf7eaecbd9f7dd6f3544525e7
Molkobain <lajarige.guillaume@free.fr>
2020-03-03 16:30:32 +0100

    :tada: First commit

---
+++ module.sample-portal-change-brick-icon.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright (C) 2013-2020 Combodo SARL
+ *
+ * This file is part of iTop.
+ *
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
+//
+// iTop module definition file
+//
+
+/** @noinspection PhpUnhandledExceptionInspection */
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'sample-portal-change-brick-icon/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Sample Portal (change the "New request" brick icon)',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-portal/2.7.0',
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(),
+		'webservice' => array(),
+		'data.struct' => array(// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(// add your sample data XML files here,
+		),
+
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(// Module specific settings go here, if any
+		),
+	)
+);
+
